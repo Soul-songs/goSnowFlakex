fac7f87e5caca725d0194d2d224ebca616da0d14
Soul-songs <viktorfrolovbn0@mail.ru>
2021-02-09 11:36:41 +0800

    add ParseId

--- uid_test.go
+++ uid_test.go
@@ -1,19 +1,41 @@
-package goSnowFlake
+package snowflake
 
 import (
 	"fmt"
 	"testing"
 )
 
+func TestTimeGen(t *testing.T) {
+	iw, _ := NewIdWorker(2)
+	ts := iw.timeGen()
+	fmt.Println(ts - CEpoch_V0)
+}
+
 func TestSnowFlake(t *testing.T) {
 	fmt.Println("start generate")
 	iw, _ := NewIdWorker(2)
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 4095; i++ {
 		id, err := iw.NextId()
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(id)
+		if i > 4090 {
+
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(id)
+				fmt.Println(ParseId(id))
+			}
+		}
+	}
+	for i := 0; i < 4095; i++ {
+		id, err := iw.NextId()
+		if i < 10 {
+
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(id)
+				fmt.Println(ParseId(id))
+			}
 		}
 	}
 	fmt.Println("end generate")
